90efc33381a3c050203c8d3a9a4950fe7bb41bbb
Milind96 <mlndshelat@gmail.com>
2020-03-18 11:49:15 +0530

    Initial commit

---
+++ Knock Knock/Knock Knock/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,22 @@
+//
+//  RegistrationController.swift
+//  Knock Knock
+//
+//  Created by milind shelat on 17/03/20.
+//  Copyright © 2020 milind shelat. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    //MARK: - Properties
+    
+    //Mark: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+}
---
+++ Knock Knock/Knock Knock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Knock Knock
+//
+//  Created by milind shelat on 17/03/20.
+//  Copyright © 2020 milind shelat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
